f103de4dc34b73c36e174d904bd3aca28c28f7f3
datarooney <datarooney>
2021-01-16 16:26:54 +0000

    fixed errors in json formatting

--- git_log_json_export.sh
+++ git_log_json_export.sh
@@ -5,9 +5,15 @@ echo "Exporting git logs to json for ${FOLDER_NAME} repo"
 # create meta folder if it doesnt exist already
 mkdir -p ../meta
 # export git log, including rowcounts and filenames where lines have been inserted or deleted.
-git log --date=iso --numstat --pretty=format:'"},{"header": "%h","author": "%an","author_date": "%ad","committer": "%cn","commit_date": "%cd","numstat": "' | sed -r 's/([0-9]{4}-[0-9]{2}-[0-9]{2})\s([0-9]{2}:[0-9]{2}:[0-9]{2})\s([-|+][0-9]{2})([0-9]{2})/\1T\2\3:\4/g' | tr -s '\r\n\t' ',,,' | \
-sed -r 's/,([0-9]+)\,([0-9]+)\,([^,"]+)/{"inserted": \1, "deleted": \2, "file_path": "\3"},/g' | \
+git log --date=iso --numstat --pretty=format:'"},{"commit_hash": "%h","author": "%an","author_date": "%ad","committer": "%cn","commit_date": "%cd","numstat": "' |
+sed -r 's/([0-9]{4}-[0-9]{2}-[0-9]{2})\s([0-9]{2}:[0-9]{2}:[0-9]{2})\s([-|+][0-9]{2})([0-9]{2})/\1T\2\3:\4/g' | tr -s '\r\n\t' ',,,' |
+sed -r 's/,-,-,[^,"]+//g' |
+sed -r 's/,([0-9]+)\,([0-9]+)\,([^,"]+)/{"inserted": \1, "deleted": \2, "file_path": "\3"},/g' |
 sed -r 's/("numstat": )"\{/\1[{/g' |
-sed -r 's/"\},,"\}/"}]}/g' > \
+sed -r 's/"\},,"\}/"}]}/g' |
+sed -r 's/^.{3}/[/g' |
+sed -r 's/.$/]/g' |
+sed -r 's/("}]},)/\1\n/g'|
+sed -r 's/(numstat": )]/\1""}]/g' > \
 ../meta/Test_${FOLDER_NAME}_git_log_output.json
 echo "export complete"

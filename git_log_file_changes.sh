3b656e8fbc143234e7ff312061eac4b8c65cc073
datarooney <datarooney>
2021-01-17 22:45:26 +0000

    removed log entry limit and removed rename entries

--- git_log_file_changes.sh
+++ git_log_file_changes.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 FOLDER_NAME=$(basename $PWD)
 # files added or deleted
-git log -n3 --name-status --pretty=format:'"}],{"commit_hash": "%h", "file_changes": [{"' |
+git log --name-status --no-renames --pretty=format:'"}],{"commit_hash": "%h", "file_changes": [{"' |
 tr -s '\r\n\t' '¬¬¬' |
 sed -r 's/\¬(.)\¬([^¬"\}]+)/file_operation": "\1", "file_path": "\2"\},\{"/g' |
+sed -r 's/\¬(R100)\¬([^¬"\}]+)/file_operation": "\1", "file_path": "\2"\},\{"/g' |
 sed -r 's/(\}\]),(\{"commit_hash")/\1\},\n\2/g' |
 sed -r 's/\},\{"¬"//g' |
 sed -r 's/,\{"\¬$/\]\}\]/g' |
-sed -r 's/^"\}\]\},$/\[/g' > \
+sed -r 's/^"\}\]\},$/\[/g' |
+sed -r 's/, "file_changes": \[\{"¬"\}\]\},$/\},/g' > \
 ../meta/${FOLDER_NAME}_file_changes.json
